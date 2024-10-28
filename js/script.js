3d0e9602d9ac8a2359503818b998cf1063274a41
raj-singhh <135631160+raj-singhh@users.noreply.github.com>
2024-10-28 17:12:09 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ function secondsToMinutesSeconds(seconds) {
 
 async function getSongs(folder) {
     currfolder = folder;
-    let a = await fetch(`http://127.0.0.1:54380/${folder}/`);//
+    let a = await fetch(`/${folder}/`);//
     let response = await a.text();
 
     let div = document.createElement("div");
@@ -68,7 +68,7 @@ const playMusic = (track, pause = false) => {
 }
 async function displayAlbums() {
     console.log("displaying albums")
-    let a = await fetch(`http://127.0.0.1:54380/songs/`)//
+    let a = await fetch(`/songs/`)//
     
     let response = await a.text();
 
@@ -83,7 +83,7 @@ async function displayAlbums() {
         if (e.href.includes("/songs/")) {
             let folder = e.href.split("/").slice(-1)[0];
             //Get the metadata of the folder
-            let a = await fetch(`http://127.0.0.1:54380/songs/${folder}/info.json`);//
+            let a = await fetch(`/songs/${folder}/info.json`);//
 
             let response = await a.json();
             cardContainer.innerHTML =cardContainer.innerHTML +  ` <div data-folder="${folder}" class="card">
